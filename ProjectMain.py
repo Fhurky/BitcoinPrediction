0d30a9cc19bb2d8adbf7ff085501d3cd4bd3b200
Fhurky <furkocal@gmail.com>
2024-09-16 22:38:53 +0300

    Updating

--- ProjectMain.py
+++ ProjectMain.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pickle as pc
 
-new_data = pd.DataFrame([[57811, 57900, 57700]], columns=['Open', 'High', 'Low'])
+new_data = pd.DataFrame([[57600, 57900, 57700]], columns=['Open', 'High', 'Low'])
 
 # Load the Model
 with open('Multilinear_regression_model.pkl', 'rb') as file:
@@ -10,7 +10,12 @@ with open('Multilinear_regression_model.pkl', 'rb') as file:
 # Load the Model
 with open('decision_tree_model.pkl', 'rb') as file:
     Tree_model = pc.load(file)
+
+# Load the Model
+with open('RandomForest_regression_model.pkl', 'rb') as file:
+    Forest_model = pc.load(file)
    
 
 print("LinearModel: ",Multilinear_model.predict(new_data))
 print("TreeModel: ",Tree_model.predict(new_data))
+print("ForestModel: ",Forest_model.predict(new_data))
--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import pickle as pc
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-
 
 # Load the data and drop unnecessary columns
 try:
@@ -16,6 +15,9 @@ except KeyError as e:
     print(f"Missing expected column in data: {e}")
     exit()
 
+# Sample only 1% of the data
+data = data.sample(frac=0.1, random_state=0)
+
 # Separate the target variable (Y) and features (X)
 Y = data['Close']  # Define 'Close' column as the target variable
 X = data.drop(['Close'], axis=1)  # Drop 'Close' column from features
@@ -23,20 +25,20 @@ X = data.drop(['Close'], axis=1)  # Drop 'Close' column from features
 # Split the data into training and testing sets
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.3, random_state=0)
 
-# Create and train the Linear Regression model
-LM = LinearRegression()
-LM.fit(train_x, train_y)
+RF_model = RandomForestRegressor(criterion='squared_error', n_jobs=-1,)
+RF_model.fit(train_x, train_y)
 
 # Save the Model
 try:
-    with open('Multilinear_regression_model.pkl', 'wb') as file:
-        pc.dump(LM, file)
+    with open('RandomForest_regression_model.pkl', 'wb') as file:
+        pc.dump(RF_model, file)
     print("Model saved successfully.")
 except Exception as e:
     print(f"Error saving the model: {e}")
 
+
 # Make predictions using the test set
-predictions = LM.predict(test_x)
+predictions = RF_model.predict(test_x)
 
 # Evaluate and print the R2 score of the model
 print("R2-score: %.2f" % r2_score(test_y, predictions))
@@ -57,4 +59,4 @@ def prediction(unknown):
     if unknown.shape[1] != train_x.shape[1]:
         raise ValueError("Input data must have the same number of features as the training data")
     
-    return LM.predict(unknown)
+    return RF_model.predict(unknown)
